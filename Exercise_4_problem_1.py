7381b9d729a69442ba570766f70078cfed20756c
ガイア <>
2021-05-09 02:37:00 +0000

    I printed two values about 48 and 71

--- Exercise_4_problem_1.py
+++ Exercise_4_problem_1.py
@@ -22,12 +22,13 @@
 def fahr_to_celsius(temp_fahrenheit):
   converted_temp=(temp_fahrenheit-32)/1.8;
   return converted_temp
-  
+
 # ### Problem 1 tests
 # 
 # Check that the function produces correct answers for:
 # 1. What is 48° Fahrenheit in Celsius? 
 print(fahr_to_celsius(48));
+
 
 # 2. What about 71° Fahrenheit in Celsius?
 print(fahr_to_celsius(71));
